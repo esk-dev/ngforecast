cdfcd612a1ccf6a613f1b679dae52e346a85026e
Egor <eesk98@gmail.com>
2022-11-30 21:47:16 +0700

    feat(added overview)

--- ui/src/app/_services/weather.service.ts
+++ ui/src/app/_services/weather.service.ts
@@ -1,13 +1,12 @@
-import { Injectable } from "@angular/core";
-import { Observable, map, switchMap, tap } from "rxjs";
-import { ApiService } from "./api.service";
-import { LocationService } from "./location.service";
-import { Forecast, Overview, ShortWeather } from "../core/models";
-import { IconService } from "./icon.service";
-@Injectable({ providedIn: "root" })
+import { Injectable } from '@angular/core';
+import { Observable, map, switchMap, tap, Subject } from 'rxjs';
+import { ApiService } from './api.service';
+import { LocationService } from './location.service';
+import { Forecast, Overview, ShortWeather } from '../core/models';
+import { IconService } from './icon.service';
+@Injectable({ providedIn: 'root' })
 export class WeatherService {
   constructor(private _api: ApiService, private iconService: IconService) {}
-
   public getShortWeather(city: string): Observable<ShortWeather> {
     return this._api.getWeather(city).pipe(
       map((response: any) => ({
@@ -19,50 +18,59 @@ export class WeatherService {
         wind: response.current.wind_kph,
         humidity: response.current.humidity,
         feelslike: response.current.feelslike_c,
-      }))
+      })),
     );
   }
   public getOverview(city: string): Observable<Overview> {
     return this._api.getWeather(city).pipe(
       map((response: any) => {
-        const { pressure_mb, wind_kph, wind_dir, uv, vis_km } =
-          response.current;
+        const { pressure_mb, wind_kph, wind_dir, uv, vis_km, precip_mm, cloud } = response.current;
         return {
           wind: {
             icon: this.iconService.registerIcon(
-              "/assets/icons/weather/wi-wind-beaufort-0.svg",
-              "windIcon"
+              '/assets/icons/weather/wi-wind-beaufort-0.svg',
+              'windIcon',
             ),
             values: wind_kph,
-            name: "wind",
+            name: 'wind',
             properties: wind_dir,
           },
           pressure: {
             icon: this.iconService.registerIcon(
-              "/assets/icons/weather/wi-barometer.svg",
-              "pressureIcon"
+              '/assets/icons/weather/wi-barometer.svg',
+              'pressureIcon',
             ),
-            name: "pressure",
+            name: 'pressure',
             values: pressure_mb,
           },
           uv: {
-            icon: this.iconService.registerIcon(
-              "/assets/icons/weather/wi-raindrop.svg",
-              "uvIcon"
-            ),
-            name: "uv",
+            icon: this.iconService.registerIcon('/assets/icons/weather/wi-raindrop.svg', 'uvIcon'),
+            name: 'uv',
             values: uv,
           },
           visibility: {
             icon: this.iconService.registerIcon(
-              "/assets/icons/weather/wi-refresh-alt.svg",
-              "visIcon"
+              '/assets/icons/weather/wi-refresh-alt.svg',
+              'visIcon',
             ),
-            name: "visibility",
+            name: 'visibility',
             values: vis_km,
           },
+          precip: {
+            icon: this.iconService.registerIcon(
+              '/assets/icons/weather/wi-umbrella.svg',
+              'precipIcon',
+            ),
+            name: 'precipation',
+            values: precip_mm,
+          },
+          cloud: {
+            icon: this.iconService.registerIcon('/assets/icons/weather/wi-cloud.svg', 'cloudIcon'),
+            name: 'cloud',
+            values: cloud,
+          },
         };
-      })
+      }),
     );
   }
 
@@ -72,12 +80,12 @@ export class WeatherService {
         console.log(response);
         const { forecastday } = response.forecast;
         return forecastday.map((el: any) => ({
-          date: new Date(el["date_epoch"] * 1000),
+          date: new Date(el['date_epoch'] * 1000),
           icon: el.day.condition.icon,
           condition: el.day.condition.text,
           temperature: el.day.avgtemp_c,
         }));
-      })
+      }),
     );
   }
 }
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: "https://ng-wht-bck.onrender.com/api",
+  API_URL: 'http://localhost:7000/api',
 };
 
 /*
